8acbfd5dce3474c0262b3829fd8ff59ed9e608cc
Antony Inian <antony.inian@zuhlke.com>
2020-02-20 12:42:45 +0800

    add init projects

---
+++ PizzaSlices/PizzaSlices/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PizzaSlices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
